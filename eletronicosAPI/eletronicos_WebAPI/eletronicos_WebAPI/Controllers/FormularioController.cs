a1acc13a3f4b8541322463ecf72f73dcce8f63bf
Maiara Souza <maiara.souza2@senaisp.edu.br>
2021-10-28 17:04:27 -0300

    subindo

---
+++ eletronicosAPI/eletronicos_WebAPI/eletronicos_WebAPI/Controllers/FormularioController.cs
@@ -0,0 +1,93 @@
+﻿using eletronicos_WebAPI.Domains;
+using eletronicos_WebAPI.Interfaces;
+using eletronicos_WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eletronicos_WebAPI.Controllers
+{
+    //Define a resposta em json
+    [Produces("application/json")]
+
+    //Define a rota
+    [Route("api/[controller]")]
+
+    //Define que é um controlador
+    [ApiController]
+    public class FormularioController : ControllerBase
+    {
+        //Faz a instanciação da interface e atribuição a nomenclatura
+        private IFormularioRepository formulariorepository { get; set; }
+
+
+        //Faz atribuição do repository
+        public FormularioController()
+        {
+            formulariorepository = new FormularioRepository();
+        }
+
+        /// <summary>
+        /// Faz a listagem
+        /// </summary>
+        /// <returns>Lista com os formulários</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(formulariorepository.Listar());
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+
+        /// <summary>
+        /// Faz o cadastro 
+        /// </summary>
+        /// <param name="formularioCriado">Formulário cadastrado</param>
+        /// <returns>Novo formulário com suas informações</returns>
+        [HttpPost]
+        public IActionResult Cadastrar(Formulario formularioCriado)
+        {
+            try
+            {
+                formulariorepository.Create(formularioCriado);
+
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+        /// <summary>
+        /// Faz atualização
+        /// </summary>
+        /// <param name="id">Identificador</param>
+        /// <param name="novoFormulario">Formulário atualizado</param>
+        /// <returns>Formulário atualizado com suas infrmações</returns>
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Formulario novoFormulario)
+        {
+            try
+            {
+                formulariorepository.Atualizar(id, novoFormulario);
+
+                return StatusCode(204);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ eletronicosAPI/eletronicos_WebAPI/eletronicos_WebAPI/Repositories/FormularioRepository.cs
@@ -0,0 +1,48 @@
+﻿using eletronicos_WebAPI.Contexts;
+using eletronicos_WebAPI.Domains;
+using eletronicos_WebAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eletronicos_WebAPI.Repositories
+{
+    //Faz a herança com a interface
+    public class FormularioRepository : IFormularioRepository
+    {
+        //Faz a implementação do context
+        Eletronicos_CTX ctx = new Eletronicos_CTX();
+
+        /// <summary>
+        /// Faz a atuaização dos formulários
+        /// </summary>
+        /// <param name="id">Identificador do formulário</param>
+        /// <param name="novoFormulario">Formulário atualizado com suas informações</param>
+        public void Atualizar(int id, Formulario novoFormulario)
+        {
+            ctx.Formularios.Update(novoFormulario);
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Faz a criação do formulário
+        /// </summary>
+        /// <param name="formularioCriado">Novo formulário com suas informações</param>
+        public void Create(Formulario formularioCriado)
+        {
+            ctx.Add(formularioCriado);
+
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Faz a listagem dos formulários
+        /// </summary>
+        /// <returns>Listagem dos formulários com suas informações</returns>
+        public List<Formulario> Listar()
+        {
+            return ctx.Formularios.ToList();
+        }
+    }
+}
