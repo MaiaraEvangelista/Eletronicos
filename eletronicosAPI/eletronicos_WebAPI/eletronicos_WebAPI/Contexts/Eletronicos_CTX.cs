05906154619ea4faf0ce052c39b86d8931428709
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-10-28 17:50:41 -0300

    subindo

--- eletronicosAPI/eletronicos_WebAPI/eletronicos_WebAPI/Contexts/Eletronicos_CTX.cs
+++ eletronicosAPI/eletronicos_WebAPI/eletronicos_WebAPI/Contexts/Eletronicos_CTX.cs
@@ -30,8 +30,8 @@ namespace eletronicos_WebAPI.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Data Source=LAPTOP-II7UP0KL; initial catalog=Eletronicos; user Id=sa; pwd=Senai@132;");
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-BOQR1F0; initial catalog=Eletronicos; user Id=sa; pwd=6829ma648;");
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-II7UP0KL; initial catalog=Eletronicos; user Id=sa; pwd=Senai@132;");
+                //optionsBuilder.UseSqlServer("Data Source=DESKTOP-BOQR1F0; initial catalog=Eletronicos; user Id=sa; pwd=6829ma648;");
             }
         }
 
